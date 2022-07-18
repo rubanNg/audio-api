e2cf76833bdf5de111abb22b9ab8e2768c222a81
Александр Рубан <ruban.mail1991@gmail.com>
2022-07-18 20:06:59 +0200

    wd

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,9 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('html');
   
+  
   await app.listen(process.env.PORT || 8080, () => {
-    console.log(`=> started on https://localhost:5000`)
+    console.log(`=> started on https://localhost:${process.env.PORT || 8080}`)
   });
 }
 bootstrap();
